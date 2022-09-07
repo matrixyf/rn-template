437c50f561b571b16061bf5f4382f24f4bb06163
zhanghong <hong.zhang@axinan.com>
2022-09-07 16:37:24 +0800

    FEAT: add react-intl

--- src/features/couter/index.tsx
+++ src/features/couter/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useState } from "react";
+import { FormattedMessage, useIntl } from 'react-intl';
 import { Button, Text, TextInput, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
+import { SetAppLocaleContext } from '../../locales/index'; 
 import { AppDispatch, RootState } from "../../store";
 import { decrement, incrementAsync } from "../../store/slice/counterSlice";
 import styles from './styles';
@@ -11,6 +13,8 @@ export default function Counter() {
   const count = useSelector((state: RootState) => state.counter.count);
   const [step, setStep] = useState('1');
   const dispatch = useDispatch<AppDispatch>();
+  const intl = useIntl();
+  const setAppLocale= useContext(SetAppLocaleContext);
 
   function dec() {
     dispatch(decrement());
@@ -50,7 +54,9 @@ export default function Counter() {
       </View>
       <View style={{flexDirection: 'row',}}>
         <Text style={{fontSize: 24}}>
-          Step:
+          <FormattedMessage
+            id='Step:'
+          />
         </Text>
         <TextInput
           style={{width: 50, textAlign: 'center', fontSize: 24}}
@@ -61,15 +67,23 @@ export default function Counter() {
         />
       </View>
       <Button
-        title='increment by step'
+        title={intl.formatMessage({id: 'increment by step'})}
         onPress={() => {
           inc(Number(step) || 1);
         }}
       />
       <Button
-        title='increment async'
+        title={intl.formatMessage({id: 'increment async'})}
         onPress={() => {
           incAsync(Number(step) || 1);
+        }}
+      />
+      <Button
+        title={intl.formatMessage({id: 'change locale to id-ID'})}
+        onPress={() => {
+          if (setAppLocale) {
+            setAppLocale('id-ID');
+          }
         }}
       />
     </View>
--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -4,17 +4,18 @@
  * @Description:
  */
 import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
 import { Text, View } from 'react-native';
 import styles from './styles';
 
 export default function HomeScreen() {
 
-  const navigation = useNavigation();
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
   return (
     <View style={styles.container}>
       <Text onPress={() => {
-        navigation.navigate('Counter', {});
+        navigation.navigate('Counter');
       }}>
         Home Screen
       </Text>
